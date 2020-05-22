23deadc47ecd814c730c29f9c2352a4bbbfceaf4
jcariaga <jay_jay_xray@yahoo.com>
2020-05-21 21:22:21 -0400

    more stuff

---
+++ app/src/main/java/com/example/grouporganizer/Home_page.java
@@ -0,0 +1,17 @@
+package com.example.grouporganizer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Home_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homepage);
+    }
+
+}
--- app/src/main/java/com/example/grouporganizer/MainActivity.java
+++ app/src/main/java/com/example/grouporganizer/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.grouporganizer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loginpage);
     }
+
+    //function called for the send button
+    public void checkLogin(View view){
+        Intent intent = new Intent(this, Home_page.class);
+        EditText user = (EditText) findViewById(R.id.username);
+        String checkuser = user.getText().toString();
+        EditText pass = (EditText) findViewById(R.id.password);
+        String checkpass = pass.getText().toString();
+        System.out.println(checkpass + checkuser);
+        //if(checkuser == "jason" && checkpass == "c"){
+
+        //    startActivity(intent);
+        //}
+        startActivity(intent);
+
+
+    }
+
+
+
 }
