96b734591fca5d30086ae9b44dd058256040ab9d
jcariaga <jay_jay_xray@yahoo.com>
2020-05-27 12:47:55 -0400

    quick changes before db implementation

--- app/src/main/java/com/example/grouporganizer/MainActivity.java
+++ app/src/main/java/com/example/grouporganizer/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     //function called for the send button
     public void checkLogin(View view){
         Intent intent = new Intent(this, Home_page.class);
-        EditText user = (EditText) findViewById(R.id.username);
+        EditText user = (EditText) findViewById(R.id.email);
         String checkuser = user.getText().toString();
         EditText pass = (EditText) findViewById(R.id.password);
         String checkpass = pass.getText().toString();
